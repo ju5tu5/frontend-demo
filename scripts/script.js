c3b466d911007c1e9c3d9149dc307697697d3404
ju5tu5 <justus@ju5tu5.nl>
2020-11-26 09:43:27 +0100

    Demo voor frontend gemaakt
    Ik heb laten zien hoe je een basisopzet voor een pagina maakt en hoe je met .js classes op een HTML element manipuleert.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,12 @@
 // JavaScript Document
+const demoParagraaf = document.querySelector('#demoParagraaf')
+
+demoParagraaf.addEventListener('mouseenter', toevoegen)
+demoParagraaf.addEventListener('mouseleave', verwijderen)
+
+function toevoegen () {
+  demoParagraaf.classList.add('anders')
+}
+function verwijderen () {
+  demoParagraaf.classList.remove('anders')
+}
